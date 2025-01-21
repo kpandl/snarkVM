815a4dfeec0f8aae47b3b04ee7b2f61f85fa10d2
Konstantin Pandl <konstantinpandl@yahoo.de>
2025-01-21 17:14:40 +0100

    clean up match arms

--- synthesizer/process/src/stack/call/mod.rs
+++ synthesizer/process/src/stack/call/mod.rs
@@ -283,7 +283,8 @@ impl<N: Network> CallTrait<N> for Call<N> {
                         // Return the request and response.
                         (request, response)
                     }
-                    CallStack::Synthesize(_, private_key, ..) => {
+                    // In Synthesize mode (with an existing proving key) or CheckDeployment mode, we generate dummy outputs to avoid building a full sub-circuit.
+                    CallStack::Synthesize(_, private_key, _) | CallStack::CheckDeployment(_, private_key, ..) => {
                         // Compute the request.
                         let request = Request::sign(
                             &private_key,
@@ -335,6 +336,7 @@ impl<N: Network> CallTrait<N> for Call<N> {
                             })
                             .collect::<Vec<_>>();
 
+                        // Execute the request.
                         let response = crate::Response::new(
                             request.network_id(),
                             substack.program().id(),
@@ -347,8 +349,10 @@ impl<N: Network> CallTrait<N> for Call<N> {
                             &output_registers,
                         )?;
 
+                        // Return the request and response.
                         (request, response)
                     }
+                    // In PackageRun mode, we sign and execute the request once.
                     CallStack::PackageRun(_, private_key, ..) => {
                         // Compute the request.
                         let request = Request::sign(
@@ -369,72 +373,6 @@ impl<N: Network> CallTrait<N> for Call<N> {
 
                         // Evaluate the request.
                         let response = substack.execute_function::<A, _>(call_stack, console_caller, root_tvk, rng)?;
-
-                        // Return the request and response.
-                        (request, response)
-                    }
-                    CallStack::CheckDeployment(_, private_key, ..) => {
-                        // Compute the request.
-                        let request = Request::sign(
-                            &private_key,
-                            *substack.program_id(),
-                            *function.name(),
-                            inputs.iter(),
-                            &function.input_types(),
-                            root_tvk,
-                            is_root,
-                            rng,
-                        )?;
-
-                        // Compute the address.
-                        let address = Address::try_from(&private_key)?;
-                        // Sample dummy outputs
-                        let outputs = function
-                            .outputs()
-                            .iter()
-                            .map(|output| match output.value_type() {
-                                ValueType::Record(record_name) => {
-                                    // Get the register index containing the record.
-                                    let index = match output.operand() {
-                                        Operand::Register(Register::Locator(index)) => Field::from_u64(*index),
-                                        _ => bail!("Expected a `Register::Locator` operand for a record output."),
-                                    };
-                                    // Compute the encryption randomizer as `HashToScalar(tvk || index)`.
-                                    let randomizer = N::hash_to_scalar_psd2(&[*request.tvk(), index])?;
-                                    // Construct the record nonce.
-                                    let record_nonce = N::g_scalar_multiply(&randomizer);
-                                    Ok(Value::Record(substack.sample_record(
-                                        &address,
-                                        record_name,
-                                        record_nonce,
-                                        rng,
-                                    )?))
-                                }
-                                _ => substack.sample_value(&address, output.value_type(), rng),
-                            })
-                            .collect::<Result<Vec<_>>>()?;
-                        // Map the output operands to registers.
-                        let output_registers = function
-                            .outputs()
-                            .iter()
-                            .map(|output| match output.operand() {
-                                Operand::Register(register) => Some(register.clone()),
-                                _ => None,
-                            })
-                            .collect::<Vec<_>>();
-
-                        // Compute the response.
-                        let response = crate::Response::new(
-                            request.network_id(),
-                            substack.program().id(),
-                            function.name(),
-                            request.inputs().len(),
-                            request.tvk(),
-                            request.tcm(),
-                            outputs,
-                            &function.output_types(),
-                            &output_registers,
-                        )?;
 
                         // Return the request and response.
                         (request, response)
