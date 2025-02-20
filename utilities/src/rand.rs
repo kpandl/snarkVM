bc34201167bb8c8cd9dfb22359de1b9c5a3f04ac
Konstantin Pandl <konstantinpandl@yahoo.de>
2025-02-20 13:28:44 +0100

    add locator

--- utilities/src/rand.rs
+++ utilities/src/rand.rs
@@ -73,7 +73,7 @@ impl TestRng {
     /// Helper to increment the trigger counter and log the current count.
     fn log_trigger(&mut self) {
         self.trigger_count += 1;
-        println!("TestRng triggered: count = {}", self.trigger_count);
+        println!("TestRng triggered: count = {}, locator = {}", self.trigger_count, std::backtrace::Backtrace::force_capture());
     }
 
     /// Returns a randomly-sampled `String`, given the maximum size in bytes and an RNG.
